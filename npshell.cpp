ce642f044dbfd073d5790695f4051b3fff418033
pflin <welin9211@gmail.com>
2024-03-21 16:24:20 +0800

    Implement Build-in Commands

--- npshell.cpp
+++ npshell.cpp
@@ -1,16 +1,47 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <vector>
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <fcntl.h>
 
 using namespace std;
 
+vector<string> parseCommand(const string& command) {
+    vector<string> cmdList;
+    string token;
+    stringstream ss(command);
+    while (ss >> token) {
+        cmdList.push_back(token);
+    }
+    ss.clear();
+    ss.str("");
+    return cmdList;
+}
+
+void build_in_command(const string& command) {
+    vector<string> cmd = parseCommand(command);
+    if (cmd[0] == "setenv") {
+        setenv(cmd[1].c_str(), cmd[2].c_str(), 1);
+    }
+    else if (cmd[0] == "printenv") {
+        const char* env = getenv(cmd[1].c_str());
+        if (env != NULL) {
+            cout << env << endl;
+        }
+    }
+    else if (cmd[0] == "exit") {
+        exit(0);
+    }
+}
+
 // Function to execute a command
 void executeCommand(const string& command) {
     // TODO: Implement command execution logic here
+    build_in_command(command);
 }
 
 // Function to handle ordinary pipe
@@ -30,20 +61,14 @@ void handleFileRedirection(const string& command, const string& fileName, bool a
 
 int main() {
     string input;
+    vector<string> commands;
+
+    setenv("PATH", "bin:.", 1); // initial PATH is bin/ and ./
 
     while (true) {
-        // Get user input
-        cout << "npshell> ";
+        cout << "% "; // Use "% " as the command line prompt.
         getline(cin, input);
 
-        // Check for exit command
-        if (input == "exit") {
-            break;
-        }
-
-        // TODO: Parse the input and handle different features accordingly
-
-        // Example: Execute a command
         executeCommand(input);
     }
 
